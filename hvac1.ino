4b167d73548d79cb41950638c0b3de731251ff15
hillselectronics <hillselectronicscompany@gmail.com>
2024-05-29 15:29:00 +0200

    Update hvac1.ino
    adding company names and product names in the code

--- hvac1.ino
+++ hvac1.ino
@@ -8,7 +8,12 @@
 #define FAN_PIN 17
 #define MIN_TEMPERATURE 10 // Minimum temperature set
 #define MAX_TEMPERATURE 12 // Maximum temperature set
-//Creating degree symbol 
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// Custom character for degree symbol
 byte degreeSymbol[8] = {
   0b00111,
   0b00101,
@@ -20,10 +25,6 @@ byte degreeSymbol[8] = {
   0b00000
 };
 
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
 void setup() {
   pinMode(COMPRESSOR_PIN, OUTPUT);
   pinMode(FAN_PIN, OUTPUT);
@@ -31,10 +32,28 @@ void setup() {
   sensors.begin();
   lcd.init();
   lcd.backlight();
-    // Create custom degree symbol
+  
+  // Create custom degree symbol
   lcd.createChar(0, degreeSymbol);
+  
   Serial.begin(115200);
   while (!Serial);
+
+  // Display company name
+  lcd.setCursor(0, 0);
+  lcd.print("Hills ELECTRONICS");
+  lcd.setCursor(0, 1);
+  lcd.print("S LTD");
+  delay(3000); // Wait for 3 seconds
+  
+  // Clear the display and show the next message
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("HVAC Trainer kit");
+  delay(3000); // Wait for another 3 seconds
+
+  // Clear the display before starting the loop
+  lcd.clear();
 }
 
 void loop() {

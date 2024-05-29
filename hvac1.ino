759d6e8e83c82492c895d13aa6a541ba59f3b4f4
hillselectronics <hillselectronicscompany@gmail.com>
2024-05-29 15:10:37 +0200

    Update hvac1.ino
    adding compressor status on lcd

--- hvac1.ino
+++ hvac1.ino
@@ -8,6 +8,17 @@
 #define FAN_PIN 17
 #define MIN_TEMPERATURE 10 // Minimum temperature set
 #define MAX_TEMPERATURE 12 // Maximum temperature set
+//Creating degree symbol 
+byte degreeSymbol[8] = {
+  0b00111,
+  0b00101,
+  0b00111,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000
+};
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -20,7 +31,8 @@ void setup() {
   sensors.begin();
   lcd.init();
   lcd.backlight();
-  
+    // Create custom degree symbol
+  lcd.createChar(0, degreeSymbol);
   Serial.begin(115200);
   while (!Serial);
 }
@@ -31,16 +43,22 @@ void loop() {
 
   Serial.print(tempC);
   lcd.setCursor(0, 0);
-  lcd.print("Temperature");
-  lcd.setCursor(0, 1);
-  lcd.print(tempC);
+  lcd.print("COIL Temp:");
+  lcd.setCursor(10, 0);
+  lcd.print(tempC,1);
+  lcd.write(byte(0));  // Display the custom degree symbol
+  lcd.print("C");
 
   if (tempC <= MIN_TEMPERATURE) {
     digitalWrite(COMPRESSOR_PIN, LOW);
     digitalWrite(FAN_PIN, HIGH);
+    lcd.setCursor(0, 1); // Set cursor to the beginning of the second line
+    lcd.print("Compressor: OFF ");
   } else {
     digitalWrite(COMPRESSOR_PIN, HIGH);
     digitalWrite(FAN_PIN, HIGH);
+    lcd.setCursor(0, 1); // Set cursor to the beginning of the second line
+    lcd.print("Compressor: ON");
   }
 
   Serial.print(tempC);
